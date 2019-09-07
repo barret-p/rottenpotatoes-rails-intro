a2a35958ebdb968d6dfe8d8148384242863996c3
barretp <barretp@tamu.edu>
2019-09-07 13:36:31 -0500

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,15 +11,23 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    end
     if params[:sort] == 'title'
-      @movies = Movie.order('title ASC')
+      session[:ratings] = @selected_ratings
+      @movies = Movie.where(rating: @selected_ratings.keys).order('title ASC')
       @title_hilite = 'hilite'
     elsif params[:sort] == 'release_date'
-      @movies = Movie.order('release_date ASC')
+      session[:ratings] = @selected_ratings
+      @movies = Movie.where(rating: @selected_ratings.keys).order('release_date ASC')
       @release_date_hilite = 'hilite'
     else
-      @movies = Movie.all
+      @movies = Movie.with_ratings(params[:ratings])
     end
+
   end
 
   def new
@@ -48,6 +56,14 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+
+  def check
+    if params[:ratings]
+      params[:ratings].keys
+    else
+      @all_ratings
+    end
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ['G','PG','PG-13','R']
+  end
+
+  def self.with_ratings(ratings)
+    if ratings == nil
+      Movie.all
+    else
+      Movie.where(rating: ratings.keys)
+    end
+  end
 end
